278e01bcc7ca4af950c774e1ac4ddd9f7e5153db
dev0piyush <dev0piyush@gmail.com>
2023-07-21 15:23:29 +0530

    phase 1

--- Utilities/_init.php
+++ Utilities/_init.php
@@ -6,6 +6,10 @@ $STUDENT='student';
 $ADMIN='admin';
 $FACULTY='faculty';
 $USER='root';
-$PASSWORD='Piyush@123'
+$PASSWORD='Piyush@123';
+$STUD_EMAIL='semail';
+$STUD_PASSWORD='spassword';
+$STUD_FNAME='sfname';
+$STUD_LNAME='slname';
 
 ?>
